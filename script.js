ca4a99dcd7c8bcca1c261c4530f73e7c212cad76
ewerton <ewerton.lima@sptech.school>
2022-05-22 17:23:10 -0300

    site atualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+function cadastrar{
+
+    var email = email.value
+    var senha = Number(input_senha.value)
+
+    if(senha.length <9){
+        resultado.innerHTML =  `Senha Inválida Menor que 8 Caracteres`
+    }
+}
