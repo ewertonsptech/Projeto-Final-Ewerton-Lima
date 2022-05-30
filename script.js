d64b753fad876cb207542fb2be84d53de6ef17f0
Ewerton Lima <ewerton.lima@sptech.school>
2022-05-30 13:45:41 -0300

    Api Site

--- script.js
+++
@@ -1,9 +0,0 @@
-function cadastrar{
-
-    var email = email.value
-    var senha = Number(input_senha.value)
-
-    if(senha.length <9){
-        resultado.innerHTML =  `Senha Inválida Menor que 8 Caracteres`
-    }
-}
