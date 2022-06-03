fd7b727d98e9c9eff3a733d73770a0ca9529d2b6
william <william.matos@sptech.school>
2022-06-03 16:04:44 -0300

    att logando

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -12,20 +12,20 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    select count(usuario.fkpersonagemfavorito) as voto , personagem.nome as personagem , usuario. *  from usuario inner join personagem on fkpersonagemfavorito = idPersonagem WHERE email = '${email}' AND senha = '${senha}'
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha , FkPersonagemFavorito) {
+function cadastrar(nome, email, senha , fkpersonagemfavorito) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkpersonagemfavorito);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha , FkPersonagemFavorito) VALUES ('${nome}', '${email}', '${senha}' , '${fkpersonagemFavorito}');
+        INSERT INTO usuario (nome, email, senha , fkpersonagemfavorito) VALUES ('${nome}', '${email}', '${senha}' , '${fkpersonagemfavorito}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
